fc18ea79001cc2bd34fec25dd92d8b37c6d447a1
Belenov <40235643+Belenov@users.noreply.github.com>
2024-10-02 14:05:17 +0500

    Update bash.bat

--- bash.bat
+++ bash.bat
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-K8S_VERSION=v1.29.3
 CERT_MANAGER_VERSION=v1.14.4
-
 BASE_URL="https://get.stopphish.ru"
 RELEASE_MANIFEST="${BASE_URL}/packages/core/latest/manifest"
-
 DIALOG=whiptail
 
 if [ "$EUID" -ne 0 ]; then
@@ -14,22 +11,45 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
-if ! ($DIALOG --clear --title "StopPhish | Installation" --no-button "Cancel" --yesno "Proceed with installation?\n\nStopPhish will be installed to the current directory." 10 60 ); then
+if ! ($DIALOG --clear --title  "StopPhish | Installation" --no-button "Cancel" --yesno "Proceed with installation?\n\nStopphish will be installed to the current directory." 10 60 ); then
     exit;
 fi
 
-# Проверка наличия kubectl
 if ! command -v kubectl &> /dev/null; then
-    echo "kubectl не найден."
+    echo "kubectl not found. Please install kubectl and ensure it is in your PATH."
     exit 1
 fi
 
-echo "Установка Cert-Manager"
-kubectl apply -f "https://github.com/cert-manager/cert-manager/releases/download/v${CERT_MANAGER_VERSION}/cert-manager.yaml"
+if ! kubectl version &> /dev/null; then
+    echo "Unable to connect to Kubernetes cluster. Ensure kubeconfig is properly configured."
+    exit 1
+fi
+
+if [[ ! -f /etc/kubernetes/registries.yaml ]]; then
+  echo "Creating registries.yaml"
+  mkdir -p /etc/kubernetes
+  curl -s "$BASE_URL/mirror/registries.yaml" -o /etc/kubernetes/registries.yaml
+else
+  echo "Custom registry configuration already exists, skipping"
+fi
+
+if [[ $(kubectl get clusterrolebinding traefik-global-read --ignore-not-found) ]]; then
+  echo "Traefik rolebinding already exists, skipping"
+else
+  echo "Creating traefik rolebinding"
+  kubectl create clusterrolebinding --clusterrole=view --serviceaccount=kube-system:traefik traefik-global-read
+fi
+
+if [[ $(kubectl get crd certificates.cert-manager.io --ignore-not-found) ]]; then
+  echo "Cert-manager already exists, skipping"
+else
+  echo "Installing cert-manager"
+  kubectl apply -f "$BASE_URL/mirror/cert-manager/$CERT_MANAGER_VERSION/cert-manager.yaml"
+fi
 
 if [[ $(kubectl get cm -n stopphish stopphish-db --ignore-not-found) ]]; then
   echo "Backing up existing database configuration"
-  kubectl get cm -n stopphish stopphish-db -o yaml > "$(date "+%Y-%m-%d_%H-%M-%S")-stopphish-db.yaml"
+  kubectl get cm -n stopphish stopphish-db -o yaml > $(date "+%Y-%m-%d_%H-%M-%S")-stopphish-db.yaml
 fi
 
 if [[ ! -f stopphish.yaml ]]; then
@@ -37,9 +57,12 @@ if [[ ! -f stopphish.yaml ]]; then
   sed -i "s|JWT_SECRET: \"stopphish\"|JWT_SECRET: \"$(< /dev/urandom tr -dc A-Za-z0-9 | head -c32)\"|" stopphish.yaml
   sed -i "s|DB_PASSWORD: \"stopphish\"|DB_PASSWORD: \"$(< /dev/urandom tr -dc A-Za-z0-9 | head -c32)\"|" stopphish.yaml
   sed -i "s|postgres:13.4|postgres:16-alpine|" stopphish.yaml
-else 
+else
   echo "Warning: stopphish.yaml already exists, skipping download! If you want a clean installation, please remove the file and run the script again."
 fi
+
+curl -s "$BASE_URL/traefik-config.yaml" -o traefik-config.yaml
+kubectl apply -f traefik-config.yaml
 
 if [[ $(kubectl get ns stopphish --ignore-not-found) ]]; then
   echo "Stopphish namespace already exists, skipping"
@@ -61,5 +84,4 @@ $DIALOG --title "StopPhish | Congratulations" --msgbox "Installation complete! \
 
 echo -e "\n\n+-------------------------------+\n|     Installation complete!    |\n+-------------------------------+\n"
 echo -e "You can now login to:\n  URL :  http://localhost:80 \n  User:  admin@admin.com\n  Pass:  admin\n"
-
 echo -e "* You may now exit the console. *\n"
